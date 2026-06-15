7f8d555d37c2248264563eb32c0853b3700bbebb
Somesh Ramola <luffydi9890@gmail.com>
2026-06-15 10:21:04 +0530

    Added User entity and repository

---
+++ src/main/java/com/som/userservice/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.som.userservice.repository;
+
+import com.som.userservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository
+        extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(
+            String email
+    );
+}
