fa18c9e5c68a9e671c7a33548852cbd495ed5f3a
Somesh Ramola <luffydi9890@gmail.com>
2026-06-15 09:42:55 +0530

    Intitial user-service setup

---
+++ src/test/java/com/som/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.som.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
